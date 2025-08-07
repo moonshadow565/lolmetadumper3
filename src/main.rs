e8a79cd20a83a1b0ca680239cc94bc6ebc5a16f3
moonshadow565 <moonshadow565@hotmail.com>
2025-08-07 14:18:18 +0200

    make version optional

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,12 @@ const PATTERN_CLASSES: &str = r"(?s-u)\x48\x8D\x3D(....)\x48?\x89\xDE\xE8....\x4
 #[allow(dead_code)]
 const PATTERN_VERSION: &str = r"(?s-u)\x00Releases/(\d+(\.\d+)+)\x00";
 
-fn find_version(data: &[u8]) -> String {
+fn find_version(data: &[u8]) -> Option<String> {
     Regex::new(PATTERN_VERSION)
         .expect("Bad regex PATTERN_VERSION!")
         .captures(data)
         .and_then(|captures| captures.get(1))
         .map(|x| { String::from_utf8_lossy(x.as_bytes()) }.to_string())
-        .expect("Failed to find PATTERN_CLASSES!")
 }
 
 fn find_classes(data: &[u8]) -> &MetaVector {
@@ -52,7 +51,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     eprintln!("Extracting version info...");
     let version = find_version(data);
-    eprintln!("Found version: {}", version);
+    eprintln!("Found version: {:?}", version);
 
     eprintln!("Finding metaclasses...");
     let classes = find_classes(data);
@@ -64,7 +63,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     eprintln!("Processing classes...");
     let meta_info = json!({
-        "version": version,
+        "version": version.unwrap_or_else(|| "unknown".to_string()),
         "classes": meta_dump::dump_class_list(data.as_ptr() as usize, classes.slice()),
     });
 
